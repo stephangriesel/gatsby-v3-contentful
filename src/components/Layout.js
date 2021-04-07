c51f66d6b0cfa3d3675e52617b255fdec6558655
Stephan <sgriesel@gmail.com>
2021-04-07 06:15:19 +0200

    separate styles & add scss

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import NavBar from "./NavBar"
 import Footer from "./Footer"
-import "./layout.css"
+import "../assets/css/styles.css"
 
 const Layout = ({ children }) => {
   return (
