f34d153bcd2595108e2fba8c87be608070a84727
Stephan <sgriesel@gmail.com>
2021-04-07 06:36:31 +0200

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,19 @@
 import React from "react"
+import styled from "styled-components"
 
 const Footer = () => {
-  return <footer>footer</footer>
+  return (
+    <FooterWrapper>
+      <p>
+        &copy; {new Date().getFullYear()}
+        <span> Guitar Mania</span>
+      </p>
+    </FooterWrapper>
+  )
 }
+
+const FooterWrapper = styled.div`
+  //
+`
 
 export default Footer
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,15 +2,20 @@ import React from "react"
 import NavBar from "./NavBar"
 import Footer from "./Footer"
 import "../assets/css/styles.css"
+import styled from "styled-components"
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <Wrapper>
       <NavBar />
       {children}
       <Footer />
-    </>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  //
+`
 
 export default Layout
