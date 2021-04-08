3fbce9197dddcdedb9efe93a91b90b0f4980bed6
Stephan <sgriesel@gmail.com>
2021-04-08 06:50:04 +0200

    navbar start

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,43 @@
 import React from "react"
 import { Link } from "gatsby"
+import { GiHamburgerMenu } from "react-icons/gi"
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/guitars">Guitars</Link>
-          <Link to="/tags">Tags</Link>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
+    <nav className="nav-bar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="">
+            <span>GuitarMania</span>
+          </Link>
+          <button className="nav-btn">
+            <GiHamburgerMenu />
+          </button>
+        </div>
+        <div className="links show-links">
+          <Link to="/" className="nav-link" activeClassName="active-link">
+            Home
+          </Link>
+          <Link
+            to="/guitars"
+            className="nav-link"
+            activeClassName="active-link"
+          >
+            Guitars
+          </Link>
+          <Link to="/tags" className="nav-link" activeClassName="active-link">
+            Tags
+          </Link>
+          <Link to="/about" className="nav-link" activeClassName="active-link">
+            About
+          </Link>
+          <div className="nav-link contact-link">
+            <Link to="/contact" className="btn">
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
     </nav>
   )
 }
