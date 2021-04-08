afbc8e8b4f30d48b4a22665936d5bcb9f4b1c8dd
Stephan <sgriesel@gmail.com>
2021-04-08 08:53:06 +0200

    mobile menu functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,55 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import { GiHamburgerMenu } from "react-icons/gi"
 
 const NavBar = () => {
+  const [show, setShow] = useState(false)
   return (
     <nav className="nav-bar">
       <div className="nav-center">
         <div className="nav-header">
-          <Link to="">
+          <Link to="/">
             <span>GuitarMania</span>
           </Link>
-          <button className="nav-btn">
+          <button className="nav-btn" onClick={() => setShow(!show)}>
             <GiHamburgerMenu />
           </button>
         </div>
-        <div className="links show-links">
-          <Link to="/" className="nav-link" activeClassName="active-link">
+        <div className={show ? "nav-links show-links" : "nav-links"}>
+          <Link
+            to="/"
+            className="nav-link"
+            activeClassName="active-link"
+            onClick={() => setShow(false)}
+          >
             Home
           </Link>
           <Link
             to="/guitars"
             className="nav-link"
             activeClassName="active-link"
+            onClick={() => setShow(false)}
           >
             Guitars
           </Link>
-          <Link to="/tags" className="nav-link" activeClassName="active-link">
+          <Link
+            to="/tags"
+            className="nav-link"
+            activeClassName="active-link"
+            onClick={() => setShow(false)}
+          >
             Tags
           </Link>
-          <Link to="/about" className="nav-link" activeClassName="active-link">
+          <Link
+            to="/about"
+            className="nav-link"
+            activeClassName="active-link"
+            onClick={() => setShow(false)}
+          >
             About
           </Link>
           <div className="nav-link contact-link">
-            <Link to="/contact" className="btn">
+            <Link to="/contact" className="btn" onClick={() => setShow(false)}>
               Contact
             </Link>
           </div>
