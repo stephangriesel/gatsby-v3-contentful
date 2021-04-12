4ca01063a82a6739824ca698e9699938d18993df
Stephan <sgriesel@gmail.com>
2021-04-12 06:58:45 +0200

    contact & style

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,33 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
-const about = () => {
+const About = () => {
   return (
     <Layout>
-      <h2>about</h2>
+      <main className="page">
+        <section className="about-page-section">
+          <article>
+            <h2>Header</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex,
+              voluptatibus?
+            </p>
+            <Link to="/contact" className="btn">
+              Contact Us
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/guitarist.jpg"
+            alt="Guitar Player"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
 
-export default about
+export default About
