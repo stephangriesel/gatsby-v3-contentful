7b8edd0f2d2ed4f9b056c687a90f72c75b532d6b
Stephan <sgriesel@gmail.com>
2021-04-13 06:16:46 +0200

    home set up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,26 @@
 import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/Layout"
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Home</h1>
+      <main>
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/guitar.jpg"
+            alt="guitar"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          ></StaticImage>
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>Guitar Mania</h1>
+              <h4>If it is too loud, you are too old</h4>
+            </div>
+          </div>
+        </header>
+      </main>
     </Layout>
   )
 }
