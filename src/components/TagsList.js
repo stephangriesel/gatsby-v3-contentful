170eb99c36eee401a08a55a9d807a9bd5eb28fd7
Stephan <sgriesel@gmail.com>
2021-04-20 07:05:47 +0200

    helper function

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -3,7 +3,7 @@ import setupTags from "../utils/setupTags"
 
 const TagsList = ({ guitars }) => {
   const newTags = setupTags(guitars)
-  console.log("test tags", newTags)
+  console.log("test tags from taglist", newTags)
   return (
     <div>
       <h2>Tags</h2>
--- src/utils/setupTags.js
+++ src/utils/setupTags.js
@@ -1,6 +1,23 @@
 const setupTags = guitars => {
-  console.log("test utils, working!")
-  return guitars
+  const allTags = {}
+
+  guitars.forEach(guitar => {
+    guitar.content.tags.forEach(tag => {
+      if (allTags[tag]) {
+        allTags[tag] = allTags[tag] + 1
+      } else {
+        allTags[tag] = 1
+      }
+    })
+  })
+
+  const newTags = Object.entries(allTags).sort((a, b) => {
+    const [firstTag] = a
+    const [secondTag] = b
+    return firstTag.localeCompare(secondTag)
+  })
+  console.log("test new tags", newTags)
+  return newTags
 }
 
 export default setupTags
