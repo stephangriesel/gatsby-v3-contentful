df1604c9b14885ee392a1017cd733ce59c7bc2ea
Stephan <sgriesel@gmail.com>
2021-04-20 07:28:11 +0200

    set up tag page

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,12 +1,40 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { Link, graphql } from "gatsby"
+import setupTags from "../utils/setupTags"
 
-const tags = () => {
+const Tags = ({ data }) => {
+  console.log("tags page data test", data)
+  const newTags = setupTags(data.allContentfulGuitar.nodes)
   return (
     <Layout>
-      <h2>tags</h2>
+      <main className="page">
+        <section className="tags-page">
+          {newTags.map((tag, index) => {
+            const [text, value] = tag
+            return (
+              <Link to={`/${text}`} key={index} className="tag">
+                <h3>{text}</h3>
+                <p>{value} guitar(s)</p>
+              </Link>
+            )
+          })}
+        </section>
+      </main>
     </Layout>
   )
 }
 
-export default tags
+export const query = graphql`
+  {
+    allContentfulGuitar {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`
+
+export default Tags
