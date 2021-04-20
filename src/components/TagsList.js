5f5446356de77b3d20300fb6660a244faef00fc4
Stephan <sgriesel@gmail.com>
2021-04-20 07:13:18 +0200

    set up taglist

--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -1,12 +1,23 @@
 import React from "react"
 import setupTags from "../utils/setupTags"
+import { Link } from "gatsby"
 
 const TagsList = ({ guitars }) => {
   const newTags = setupTags(guitars)
   console.log("test tags from taglist", newTags)
   return (
-    <div>
-      <h2>Tags</h2>
+    <div className="tag-container">
+      <h2>Guitars</h2>
+      <div className="tags-list">
+        {newTags.map((tag, index) => {
+          const [text, value] = tag
+          return (
+            <Link to={`/${text}`} key={index}>
+              {text} ({value})
+            </Link>
+          )
+        })}
+      </div>
     </div>
   )
 }
