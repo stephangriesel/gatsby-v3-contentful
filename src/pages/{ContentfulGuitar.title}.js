ac530dc05b3dd8138159d4971b9560b56f4b82f5
Stephan <sgriesel@gmail.com>
2021-04-22 06:50:38 +0200

    add dynamic data

--- src/pages/{ContentfulGuitar.title}.js
+++ src/pages/{ContentfulGuitar.title}.js
@@ -1,8 +1,10 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { BsFillInfoCircleFill, BsFileEarmarkText } from "react-icons/bs"
 import { BiCalculator } from "react-icons/bi"
+
+import Layout from "../components/Layout"
 
 // Data in this template will be available as variable, example title query
 
@@ -17,11 +19,61 @@ const GuitarTemplate = ({ data }) => {
   const pathToImage = getImage(image) // Helper function for image
   const { productspecs, weightanddimensions, tags, basicinfo } = content // destructure content data
   return (
-    <main className="page">
-      <div className="guitar-page">
-        <h1>{title}</h1>
-      </div>
-    </main>
+    <Layout>
+      <main className="page">
+        <div className="guitar-page">
+          <section className="guitar-hero">
+            <GatsbyImage
+              image={pathToImage}
+              alt={title}
+              className="template-img"
+            />
+            <article className="guitar-info">
+              <h2>{title}</h2>
+              <p>{description}</p>
+              <div className="guitar-icons">
+                <article>
+                  <BiCalculator />
+                  <h5>Dimensions</h5>
+                  <p>{weightanddimensions}</p>
+                </article>
+                <article>
+                  <BsFileEarmarkText />
+                  <h5>Product Specs</h5>
+                  <p>{productspecs}</p>
+                </article>
+                <article>
+                  <BiCalculator />
+                  <h5>Info</h5>
+                  <p>{basicinfo}</p>
+                </article>
+              </div>
+              <p className="guitar-tags">
+                Tags:
+                {tags.map((tag, index) => {
+                  return (
+                    <Link to={`/${tag}`} key={index}>
+                      {tag}
+                    </Link>
+                  )
+                })}
+              </p>
+            </article>
+          </section>
+          <section className="guitar-content">
+            <article className="left-column">Left Column</article>
+            <article className="second-column">
+              <div>
+                <h4>data</h4>
+              </div>
+              <div>
+                <h4>more data</h4>
+              </div>
+            </article>
+          </section>
+        </div>
+      </main>
+    </Layout>
   )
 }
 
