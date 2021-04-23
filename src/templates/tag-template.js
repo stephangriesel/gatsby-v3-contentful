2d484e08eeac62992c8a3c5576bd8cc81449a5f7
Stephan <sgriesel@gmail.com>
2021-04-23 06:45:07 +0200

    set up template return for tags

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -1,11 +1,38 @@
 import React from "react"
+import { graphql } from "gatsby"
+import GuitarList from "../components/GuitarList"
+import Layout from "../components/Layout"
 
-const TagTemplate = () => {
+const TagTemplate = ({ data, pageContext }) => {
+  const guitars = data.allContentfulGuitar.nodes
+  console.log("tag template data test", data)
   return (
-    <div>
-      <h2>tag template page</h2>
-    </div>
+    <Layout>
+      <main className="page">
+        <h2>{pageContext.tag}</h2>
+        <div className="tag-guitars">
+          <GuitarList guitars={guitars} />
+        </div>
+      </main>
+    </Layout>
   )
 }
+
+export const query = graphql`
+  query GetGuitarByTag($tag: String) {
+    allContentfulGuitar(
+      sort: { order: ASC, fields: title }
+      filter: { content: { tags: { eq: $tag } } }
+    ) {
+      nodes {
+        title
+        id
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`
 
 export default TagTemplate
